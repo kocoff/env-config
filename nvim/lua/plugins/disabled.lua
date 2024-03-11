b19139ab9ef325f676060eea4d54838aeb308909
Michał Kocon <michal.kocon@n7space.com>
2024-03-11 22:28:20 +0100

    Initial commit

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  { "echasnovski/mini.ai", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "gbprod/cutlass.nvim", enabled = true },
+  { "gorbit99/codewindow.nvim", enabled = true },
+  { "mrjones2014/legendary.nvim", enabled = true },
+  { "folke/flash.nvim", enabled = false },
+  { "mrjones2014/legendary.nvim", enabled = false },
+}
---
+++ nvim/lua/plugins/legendary.lua
@@ -0,0 +1,16 @@
+return {
+  "mrjones2014/legendary.nvim",
+  -- since legendary.nvim handles all your keymaps/commands,
+  -- its recommended to load legendary.nvim before other plugins
+  priority = 10000,
+  lazy = false,
+  -- sqlite is only needed if you want to use frecency sorting
+  dependencies = { "kkharji/sqlite.lua" },
+
+  -- where you set up legendary.nvim
+  -- now the keymaps from the `flash.nvim` plugin spec will be automat`ically loaded
+
+  config = function()
+    require("legendary").setup({})
+  end,
+}
