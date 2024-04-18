039b9d52e0731c720ff80d58b96710b4fdd77218
Michał Kocon <nobooya@gmail.com>
2024-04-18 20:22:08 +0200

    Configure noice for nvim

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -12,5 +12,9 @@ return {
       enabled = true, -- enables the Noice cmdline UI
       view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
     },
+    popupmenu = {
+      enabled = true,
+      view = "cmdline",
+    },
   },
 }
